5161eab7d3e6faa173ebc234a576cec9b7d4b7cc
Kelly <krsmith35@gmail.com>
2013-01-16 16:41:54 -0600

    linkedin API experiment

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,15 @@
+
 console.log('This would be the main JS file.');
+function onLinkedInLoad() {
+     IN.Event.on(IN, "auth", onLinkedInAuth);
+}
+
+function onLinkedInAuth() {
+     IN.API.Profile("me").result(displayProfiles);
+}
+
+function displayProfiles(profiles) {
+     member = profiles.values[0];
+     document.getElementById("profiles").innerHTML = 
+          "<p id=\"" + member.id + "\">Hello " +  member.firstName + " " + member.lastName + "</p>";
+}
