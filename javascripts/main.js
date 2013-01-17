cb3526cccaf4bbe5f48dd1874d4970fa1a025bad
Kelly <krsmith35@gmail.com>
2013-01-17 10:51:15 -0600

    added a function for displaying linkedin profile

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,4 +1,3 @@
-
 console.log('This would be the main JS file.');
 function onLinkedInLoad() {
      IN.Event.on(IN, "auth", onLinkedInAuth);
@@ -13,3 +12,20 @@ function displayProfiles(profiles) {
      document.getElementById("profiles").innerHTML = 
           "<p id=\"" + member.id + "\">Hello " +  member.firstName + " " + member.lastName + "</p>";
 }
+
+/* Sample code from a tutorial about how to post profile data to html
+function loadData() {
+  IN.API.Profile("me")
+    .fields(["id", "firstName", "lastName", "pictureUrl","headline","publicProfileUrl"])
+    .result(function(result) {
+      profile = result.values[0];
+      profHTML = "<p><a href=\"" + profile.publicProfileUrl + "\">";
+      profHTML += "<img class=img_border align=\"left\" src=\"" + profile.pictureUrl + "\"></a>";      
+      profHTML += "<a href=\"" + profile.publicProfileUrl + "\">";
+      profHTML += "<h2 class=myname>" + profile.firstName + " " + profile.lastName + "</a> </h2>";
+      profHTML += "<span class=myheadline>" + profile.headline + "</span>";
+      $("#profile").html(profHTML);
+    });
+}
+*/
+
