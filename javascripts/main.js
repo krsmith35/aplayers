4be46bcb8511c82ef39daea339abe2fde70e61f5
Kelly <krsmith35@gmail.com>
2013-02-11 17:40:00 -0600

    Update javascripts/main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,45 +1,4 @@
-function onLinkedInLoad() {
-  IN.Event.on(IN, "auth", function() {onLinkedInLogin();});
-  IN.Event.on(IN, "logout", function() {onLinkedInLogout();});
-}
 
-function onLinkedInLogout() {
-  setLoginBadge(false);
-}
-
-function onLinkedInLogin() {
-  // we pass field selectors as a single parameter (array of strings)
-  IN.API.Profile("me")
-    .fields(["id", "firstName", "lastName", "pictureUrl", "publicProfileUrl", "emailAddress"])
-    .result(function(result) {
-      setLoginBadge(result.values[0]);
-    })
-    .error(function(err) {
-      alert(err);
-    });
-    
-  //setting global variables for linkedin info
-  var email = profile.emailAddress
-  
-  //firebase script to store user values
-  var user = new Firebase('https://a-players.firebaseio.com/');
-  user.push({email: email});
-}
-
-function setLoginBadge(profile) {
-  if (!profile) {
-    profHTML = "You are not logged in.";
-  }
-  else {
-    var pictureUrl = profile.pictureUrl || "http://static02.linkedin.com/scds/common/u/img/icon/icon_no_photo_80x80.png";
-    profHTML = "<p><a href=\"" + profile.publicProfileUrl + "\">";
-    profHTML = profHTML + "<img align=\"baseline\" src=\"" + pictureUrl + "\"></a>";      
-    profHTML = profHTML + "&nbsp; Welcome <a href=\"" + profile.publicProfileUrl + "\">";
-    profHTML = profHTML + profile.firstName + " " + profile.lastName + "</a>! <a href=\"#\" onclick=\"IN.User.logout(); return false;\">logout</a></p>";
-    profHTML += " Email: " + email;
-  }
-  document.getElementById("loginbadge").innerHTML = profHTML;
-}
 
 
 
